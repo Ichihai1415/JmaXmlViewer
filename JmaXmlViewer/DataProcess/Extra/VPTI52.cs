63d811474faf1ee53ef3c428e02a66856707d304
Ichihai1415 <projects3141.gif@gmail.com>
2025-06-15 16:17:11 +0900

    [分割]VPTI50~52,VGSK50仮

---
+++ JmaXmlViewer/DataProcess/Extra/VPTI52.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml.Serialization;
+using static JmaXmlViewer.Utilities.XmlClass;
+
+namespace JmaXmlViewer.DataProcess.Extra
+{
+    internal partial class Processes_Extra
+    {
+        public static void VPTI52(string xmlString)
+        {
+            try
+            {
+                var serializer = new XmlSerializer(typeof(C_Report_Common));
+                using var reader = new StringReader(xmlString);
+                var test = (C_Report_Common?)serializer.Deserialize(reader);
+                Console.WriteLine("[VPTI52] " + test?.Head.Headline.Text);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[VPTI52] ERROR: " + ex.Message);
+            }
+
+        }
+
+
+    }
+}
---
+++ JmaXmlViewer/DataProcess/Other/VGSK50.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml.Serialization;
+using static JmaXmlViewer.Utilities.XmlClass;
+
+namespace JmaXmlViewer.DataProcess.Other
+{
+    internal partial class Processes_Other
+    {
+        public static void VGSK50(string xmlString)
+        {
+            try
+            {
+                var serializer = new XmlSerializer(typeof(C_Report_Meteorological));
+                using var reader = new StringReader(xmlString);
+                var test = (C_Report_Meteorological?)serializer.Deserialize(reader);
+                //Console.WriteLine("[VGSK50] " + test?.Head.Headline.Text);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[VGSK50] ERROR: " + ex.Message);
+            }
+
+        }
+
+
+    }
+}
