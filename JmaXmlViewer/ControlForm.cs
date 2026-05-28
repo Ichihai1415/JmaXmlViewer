c5d3d684338b462e1e4e03e2d6a1a832fa0ffc05
Ichihai1415 <projects3141.gif@gmail.com>
2026-05-28 17:28:59 +0900

    電文更新のため調整

--- JmaXmlViewer/ControlForm.cs
+++ JmaXmlViewer/ControlForm.cs
@@ -281,6 +281,10 @@ namespace JmaXmlViewer
 
         //àÍéû
         internal static string[] ignoreCodes = [ "VPWW53", "VXSE51", "VXSE52", "VXSE53", "VPCU51", "VPCY51", "VPZU52","VFVO60","VPTW60",
+            //20260508~
+            "VPFD61","VPWP50",
+            //í∑Ç¢ÇÃÇ≈í‚é~/å√Ç¢ÇÃÇ≈í‚é~
+            "VPFT50","VPWW54","VXWW50",
             //regular
             "VPFG50", "VPFD50", "VPFD51", "VPZW50", "VPCW50", "VPFW50", "VPZK50", "VPCK50", "VPFD60", "VPFW60", "VZSA50", "VZSF50", "VZSF51", "VZSA60", "VZSF60", "VZSF61", "VPZK70", "VPCK70", "VPRN50" ];//âºÅAê›íËÇ≈Ç‚ÇÈ
 
